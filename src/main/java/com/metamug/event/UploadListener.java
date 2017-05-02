bae013e55a2e1715eaef1e39202e4a879a21363c
Deepak Mishra <d3ep4k@users.noreply.github.com>
2017-05-02 11:42:28 +0530

    Update UploadListener.java

--- src/main/java/com/metamug/event/UploadListener.java
+++ src/main/java/com/metamug/event/UploadListener.java
@@ -67,7 +67,9 @@ public interface UploadListener {
      *
      * @param event
      * @param dataSource DataSource object to connect with Database
+     * @return Returned object will be converted to JSON/XML depending on accept header 
+     * supplied in the multipart(upload) request
      */
-    public void uploadPerformed(UploadEvent event, DataSource dataSource);
+    public Object uploadPerformed(UploadEvent event, DataSource dataSource);
 
 }
