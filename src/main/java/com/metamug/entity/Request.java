5d84947c697a0716efecb51088e3ed1b8fbd780e
Deepak Mishra <d3ep4k@users.noreply.github.com>
2018-02-13 17:01:34 +0530

    Create Request.java

---
+++ src/main/java/com/metamug/entity/Request.java
@@ -0,0 +1,59 @@
+package com.metamug.entity;
+
+import java.util.Map;
+
+public class Request {
+
+    private final Map<String, String> params;
+    private final Map<String, String> headers;
+    private final String  method;
+    private final Resource resource;
+    
+    protected Request(Map<String, String> params, Map<String, String> headers, String method, Resource resource) {
+        this.params = params;
+        this.headers = headers;
+        this.resource = resource;
+        this.method = method;
+    }
+
+    public String getMethod(){
+        return this.method;   
+    }
+    
+    /**
+    * Get information about the requested resource
+    * @return Resource resource object
+    */
+    public Resource getResource(){
+        return this.resource;
+    }
+    
+    public String getHeader(String header) {
+        return headers.get(header);
+    }
+
+    public String getParameter(String param) {
+        return params.get(param);
+    }
+    
+    
+    /**
+    * Set Header into the request.
+    * @param key name of the header
+    * @param value of the header
+    */
+    public void setHeader(String key, String value) {
+        headers.put(key, value);
+    }
+    
+    
+    /**
+    * Set Header into the request.
+    * @param key name of the request parameter
+    * @param value of the request parameter
+    */
+    public void setParamter(String key, String value) {
+        params.put(key, value);
+    }
+    
+}
