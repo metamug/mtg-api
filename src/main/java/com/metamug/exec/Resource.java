51e1f26ea53a754fd70fe31c436efa6b8b5b68d9
Deepak Mishra <d3ep4k@users.noreply.github.com>
2018-02-13 15:17:58 +0530

    Update Resource.java

--- src/main/java/com/metamug/exec/Resource.java
+++ src/main/java/com/metamug/exec/Resource.java
@@ -1,1 +1,30 @@
+package com.metamug.exec;
 
+import java.util.Map;
+
+public class Resource {
+  
+    private final String name, role; 
+    private final float version;
+
+    protected Resource(String name, float version){
+      this.name = name;
+      this.version = version;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public float getVersion{
+        return this.version   
+    }
+    
+    /**
+    * Get the role defined in the auth attribute of the resource file.
+    */
+    public String getRole(){
+        retun this.role
+    }
+  
+}
